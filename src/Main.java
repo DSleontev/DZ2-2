e24b680f7e13a6ad11d041cfe24e50ed40ae56ef
Леонтьев Дмитрий QA-71 <leontyev.goods@gmail.com>
2023-10-30 08:19:46 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int x = 100;  // начальный баланс
+        int y = 10000; //пополнение баланса
+        int bonus;
+
+        if (y > 1000) {
+            bonus = y / 100;
+        } else {
+            bonus = 0;
+        }
+        int z = x + y + bonus;
+
+        System.out.println("Итоговый баланс "+ z);
+
     }
 }
